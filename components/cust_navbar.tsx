64b074a246435f03e93593e85869a0462d9ae29d
Abdulhamid109 <abdulhamidpatel109@gmail.com>
2026-08-12 22:08:32 +0530

    only QR endpoint will be visible on master preview

--- components/cust_navbar.tsx
+++ components/cust_navbar.tsx
@@ -66,9 +66,9 @@ const Cust_navbar = () => {
           </Link>
         ))}
 
-        {/* <div className="w-px h-4 bg-gray-200 mx-2" /> */}
+        <div className="w-px h-4 bg-gray-200 mx-2" />
 
-        {/* {session ? (
+        {session ? (
           <button
             onClick={handleLogout}
             className="text-sm text-red-500 hover:text-red-600 px-3 py-1.5 rounded-md hover:bg-red-50 transition-colors"
@@ -82,7 +82,7 @@ const Cust_navbar = () => {
           >
             Get Started
           </Link>
-        )} */}
+        )}
       </div>
 
       {/* Mobile Toggle */}
@@ -119,9 +119,9 @@ const Cust_navbar = () => {
             </Link>
           ))}
 
-          {/* <div className="w-full h-px bg-gray-100 my-1" /> */}
+          <div className="w-full h-px bg-gray-100 my-1" />
 
-          {/* {session ? (
+          {session ? (
             <button
               onClick={() => { handleLogout(); setMenuOpen(false) }}
               className="w-full text-left text-sm text-red-500 px-3 py-2 rounded-md hover:bg-red-50 transition-colors"
@@ -136,7 +136,7 @@ const Cust_navbar = () => {
             >
               Get Started
             </Link>
-          )} */}
+          )}
         </div>
       )}
     </nav>
